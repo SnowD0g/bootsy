fe11e157c9eb625238959ed6d72aa4133086a438
Marco Iannantuono <marco.iannantuono@democom.it>
2018-05-09 17:36:22 +0200

    chrome v64 fix

--- app/assets/javascripts/bootsy/vendor/bootstrap-wysihtml5.js
+++ app/assets/javascripts/bootsy/vendor/bootstrap-wysihtml5.js
@@ -276,7 +276,7 @@
                 var activeButton = $(this).hasClass("wysihtml5-command-active");
 
                 if (!activeButton) {
-                    self.editor.currentView.element.focus(false);
+                    self.editor.currentView.element.focus({});
                     caretBookmark = self.editor.composer.selection.getBookmark();
                     insertImageModal.appendTo('body').modal('show');
                     insertImageModal.on('click.dismiss.modal', '[data-dismiss="modal"]', function(e) {
